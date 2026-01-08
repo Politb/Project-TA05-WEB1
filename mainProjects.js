d8e427676234403ef91b2c7078fc8008df10ff70
polguerrero <pol.guerrero.7e7@itb.cat>
2026-01-08 08:36:08 +0100

    .

--- mainProjects.js
+++ mainProjects.js
@@ -2,7 +2,7 @@
 
 const projects = [
     {
-      title: "Primer proyecto",
+      title: "First project",
       description: "Esta es la descripción de mi primer proyecto",
       image:
         "https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d2VifGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
@@ -103,6 +103,3 @@ const ProjectCard = (project) => `
 `;
 
 Projects();
-
-
-
