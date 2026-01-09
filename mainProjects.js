0dddf91a04595b00f00126d8ea53b02318cb7575
polguerrero <pol.guerrero.7e7@itb.cat>
2026-01-09 12:39:46 +0100

    .

--- mainProjects.js
+++ mainProjects.js
@@ -1,27 +1,26 @@
-
-
 const projects = [
     {
       title: "First project",
-      description: "Esta es la descripción de mi primer proyecto",
-      image:
-        "https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d2VifGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      description: `This project integrates Fail2Ban with Discord to provide real‑time
+            security alerts. The system detects brute‑force and DoS attacks on
+            services like SSH and Apache, automatically blocks malicious IPs,
+            and sends instant notifications to a Discord channel via Webhooks.`,
+      image: "img/lifeclubmma.png",
       tech: ["React", "HTML5", "CSS3", "JavaScript"],
       github: "http://github.com/myuser/proyecto1",
       link: "www.proyecto1.com",
     },
     {
       title: "Second project",
-      description: "This is the description of my second project",
-      image:
-        "https://images.unsplash.com/photo-1661961111184-11317b40adb2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2344&q=80",
+      description: "                        A modern website designed for a Mixed Martial Arts gym, showcasing classes, schedules, trainers, and promotional content. The goal is to create a dynamic and visually engaging platform to attract new members.",
+      image: "img/fail2ban-logo-security-art-work.jpeg",
       tech: ["React", "HTML5", "CSS3", "JavaScript"],
       github: "http://github.com/myuser/proyecto2",
       link: "www.proyecto1.com",
     },
     {
       title: "Third project",
-      description: "This is the description of my third project",
+      description: "            This project is a mobile application that helps users track their fitness goals. It includes features like workout logging, progress tracking, and social sharing.",
       image:
         "https://images.unsplash.com/photo-1669570094762-828f3dfaf675?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80",
       tech: ["React", "HTML5", "CSS3", "JavaScript"],
@@ -30,7 +29,7 @@ const projects = [
     },
     {
       title: "Fourth project",
-      description: "This is the description of my fourth project",
+      description: "            This is the description of my fourth project",
       image:
         "https://images.unsplash.com/photo-1664575198308-3959904fa430?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80",
       tech: ["React", "HTML5", "CSS3", "JavaScript"],
@@ -39,7 +38,7 @@ const projects = [
     },
     {
       title: "Fifth project",
-      description: "This is the description of my fifth project",
+      description: "            This is the description of my fifth project",
       image:
         "https://images.unsplash.com/photo-1665686306574-1ace09918530?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3387&q=80",
       tech: ["React", "HTML5", "CSS3", "JavaScript"],
